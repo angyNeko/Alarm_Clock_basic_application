2d48ba4b19cd1e6b1ef8071d505914e16fc1285b
Nekofi <58923014+LolisDisWae@users.noreply.github.com>
2022-01-27 12:46:46 +0800

    Backbone Ready

--- app/src/main/java/com/example/alarmclockbasicapplication/MainActivity.java
+++ app/src/main/java/com/example/alarmclockbasicapplication/MainActivity.java
@@ -1,47 +1,45 @@
 package com.example.alarmclockbasicapplication;
 
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import java.util.EventListener;
+import com.example.alarmclockbasicapplication.ui.main.SectionsPagerAdapter;
+import com.example.alarmclockbasicapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button setAlarm, manageTime;
-    private TextView timeV;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        getSupportActionBar().hide();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        // go to setalarm
-        setAlarm = (Button) findViewById(R.id.setAlarm);
-        setAlarm.setOnClickListener(new View.OnClickListener() {
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
+
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent toSetA = new Intent(MainActivity.this, SetAlarmActivity.class);
-                startActivity(toSetA);
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
-
-        // go to managetime
-        manageTime = (Button) findViewById(R.id.manageTime);
-        manageTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent toManageT = new Intent(MainActivity.this, ManageTimeActivity.class);
-                startActivity(toManageT);
-            }
-        });
-
     }
 }
